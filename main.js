1eeae2fe6b574e791109f4b8b2c427fee79f17d3
Panchajanya HR <panchajanyahr@gmail.com>
2013-12-29 08:53:16 +0100

    Added color table

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 var allTickers = [];
 var fadeDuration = 500;
+var colorTable = ["#3366cc","#dc3912","#ff9900","#109618","#990099","#0099c6","#dd4477","#66aa00","#b82e2e","#316395","#994499","#22aa99","#aaaa11","#6633cc","#e67300","#8b0707","#651067","#329262","#5574a6","#3b3eac","#b77322","#16d620","#b91383","#f4359e","#9c5935","#a9c413","#2a778d","#668d1c","#bea413","#0c5922","#743411"];
 
 function processCsv(path, callback) {
 	$.ajax({
@@ -67,6 +68,7 @@ function addToBasket(tag) {
 	var tagNode = $("<li/>");
 	tagNode.append(nameNode);
 	tagNode.append(closeNode);
+	tagNode.css('background-color', colorTable[$("ul.basket li").length]);
 	tagNode.prop('_data', data);
 
 	tagNode.appendTo($("ul.basket")).hide().fadeIn(fadeDuration);
