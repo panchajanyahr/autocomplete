299205dd6a857def3e187b743dda948b274df049
Panchajanya HR <panchajanyahr@gmail.com>
2013-12-31 09:44:55 +0100

    Using full csv

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ var fadeDuration = 500;
 var colorTable = ["#3366cc","#dc3912","#ff9900","#109618","#990099","#0099c6","#dd4477","#66aa00","#b82e2e","#316395","#994499","#22aa99","#aaaa11","#6633cc","#e67300","#8b0707","#651067","#329262","#5574a6","#3b3eac","#b77322","#16d620","#b91383","#f4359e","#9c5935","#a9c413","#2a778d","#668d1c","#bea413","#0c5922","#743411"];
 
 $(function() {
-	processCsv('List.csv', function(rows) {
+	processCsv('FullList.csv', function(rows) {
 		$('.ui.dimmer').remove();
 		allTickers = $.grep(rows, function(row) {
 			return !isNaN(marketCapValue(row["Market Cap"]));
